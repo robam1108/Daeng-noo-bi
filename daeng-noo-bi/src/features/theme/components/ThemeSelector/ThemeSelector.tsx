3da4b93a36baa1d3357fa9faad387421ef896f3a
orinery <gucheahyun@gmail.com>
2025-05-02 21:36:37 +0900

    폴더 정리

--- daeng-noo-bi/src/features/theme/components/ThemeSelector/ThemeSelector.tsx
+++ daeng-noo-bi/src/features/theme/components/ThemeSelector/ThemeSelector.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ThemeInfo, THEMES } from '../constants/themeConstants';
-import './ThemeSelector.scss';
+import React from "react";
+import { ThemeInfo, THEMES } from "../../constants/themeConstants";
+import "./ThemeSelector.scss";
 
 interface Props {
-  selectedTheme: ThemeInfo['key'];
-  onSelect: (theme: ThemeInfo['key']) => void;
+  selectedTheme: ThemeInfo["key"];
+  onSelect: (theme: ThemeInfo["key"]) => void;
 }
 
 export default function ThemeSelector({ selectedTheme, onSelect }: Props) {
@@ -13,7 +13,9 @@ export default function ThemeSelector({ selectedTheme, onSelect }: Props) {
       {THEMES.map(({ key, title, className }) => (
         <button
           key={key}
-          className={`theme-selector-button ${className} ${selectedTheme === key ? 'active' : ''}`}
+          className={`theme-selector-button ${className} ${
+            selectedTheme === key ? "active" : ""
+          }`}
           onClick={() => onSelect(key)}
           aria-label={title}
         >
