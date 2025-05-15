be25c80a83f25cd1fc3ef5a11b208e36e89f1ac8
orinery <gucheahyun@gmail.com>
2025-05-15 13:05:36 +0900

    (0515)빌드전 환경 설정

--- daeng-noo-bi/vite.config.ts
+++ daeng-noo-bi/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
-      // 브라우저에서 /api/* 로 들어오는 요청을
-      // https://apis.data.go.kr/B551011/* 로 포워딩합니다.
+      // 개발 환경에서만 작동 (Vercel 배포 후엔 사용되지 않음)
+      // 브라우저에서 /api/* 로 들어오는 요청을 https://apis.data.go.kr/B551011/* 로 포워딩합니다.
       '/api': {
         target: 'https://apis.data.go.kr/B551011',
         changeOrigin: true,
