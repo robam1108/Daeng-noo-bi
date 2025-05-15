d1d56a1ed4f90033e17960ab934bf6a4c2457c8d
boram <108602668+robam1108@users.noreply.github.com>
2025-05-15 15:35:40 +0900

    에러페이지 수정

--- daeng-noo-bi/src/shared/components/Error/Error.tsx
+++ daeng-noo-bi/src/shared/components/Error/Error.tsx
@@ -2,19 +2,23 @@ import "./Error.scss";
 
 interface Props {
     error?: Error;
+    errorMessage?: string;
     onRetry?: () => void;
 }
 
-export default function Error({ error, onRetry }: Props) {
+export default function Error({ error, onRetry, errorMessage }: Props) {
     return (
-        <div>
-            <div className="Error">
-                <div className="error-icon">⚠️</div>
-                <h2>문제가 발생했습니다.</h2>
-                <p>죄송합니다. 요청을 처리하는 중 오류가 발생했어요.</p>
-                {error && <pre className="error-message">{error.message}</pre>}
-                <button onClick={onRetry}>다시 시도하기</button>
-            </div>
+        <div className="Error">
+            <div className="error-icon">⚠️</div>
+            {
+                errorMessage ? <h2>{errorMessage}</h2> :
+                    <div>
+                        <h2>문제가 발생했습니다.</h2>
+                        <p>죄송합니다. 요청을 처리하는 중 오류가 발생했어요.</p>
+                    </div>
+            }
+            {error && <pre className="error-message">{error.message}</pre>}
+            {onRetry && <button onClick={onRetry}>다시 시도하기</button>}
         </div>
     )
 }
