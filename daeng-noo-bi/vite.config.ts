444cc5fd467523274121180b842b358158b18782
orinery <gucheahyun@gmail.com>
2025-05-19 22:27:26 +0900

    배포 후 새로고침 문제 수정

--- daeng-noo-bi/vite.config.ts
+++ daeng-noo-bi/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       // /KorPetTourService/* 요청을 CF로 프록시
